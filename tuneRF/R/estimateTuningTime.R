8fa58e511912761fb74a436dcdd0ffbab692d4e6
PhilippPro <philipp_probst@gmx.de>
2017-06-30 15:36:45 +0200

    change estimateTuneRangerTime

--- tuneRF/R/estimateTuningTime.R
+++ tuneRF/R/estimateTuningTime.R
@@ -1,6 +1,7 @@
 
-estimateTuneRangerTime = function(formula, data, iters = 100, num.threads = 1) {
-  time = system.time(ranger(formula, data, num.trees = 1000, num.threads))[3]
+estimateTuneRangerTime = function(formula, data, iters = 100, num.threads = 1, num.trees = 1000, respect.unordered.factors = TRUE) {
+  time = system.time(ranger(formula, data, num.threads = num.threads, 
+    num.trees = num.trees, respect.unordered.factors = respect.unordered.factors))[3]
   cat(paste("Time estimation is", my_seconds_to_period(time * iters)))
 }
 
