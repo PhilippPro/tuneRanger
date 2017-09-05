4a0863a74362ab6c23a2b292c22cb97975ba1537
PhilippPro <philipp_probst@gmx.de>
2017-09-05 16:38:05 +0200

    include time estimation

--- benchmark.R
+++ benchmark.R
@@ -26,13 +26,27 @@ tasks = listOMLTasks(number.of.classes = 2L, number.of.missing.values = 0,
   data.tag = "study_14", estimation.procedure = "10-fold Crossvalidation")
 task.ids = tasks$task.id
 
-rdesc = makeResampleDesc("RepCV", reps = 2, folds = 2)
+rdesc = makeResampleDesc("RepCV", reps = 2, folds = 5)
 measures = list(mmce, multiclass.au1p, multiclass.brier, logloss, timetrain)
 
+# time estimation
+time.estimate = list()
+for(i in seq_along(task.ids)) {
+  print(i)
+  task = getOMLTask(task.ids[i])
+  task = convertOMLTaskToMlr(task)$mlr.task
+  time.estimate[[i]] = estimateTuneRFTime(task)
+  print(time.estimate[[i]])
+}
+save(time.estimate, file = "./benchmark/time.estimate.RData")
+# benchmark
 bmr = list()
 for(i in seq_along(task.ids)) {
-  task = getOMLTask(task.ids[i])
-  task = convertOMLTaskToMlr(task)
-  bmr[[i]] = benchmark(lrns, task, rdesc, measures, keep.pred = FALSE, models = FALSE)
+  if(time.estimate[[i]] < 300){
+    task = getOMLTask(task.ids[i])
+    task = convertOMLTaskToMlr(task)$mlr.task
+    bmr[[i]] = benchmark(lrns, task, rdesc, measures, keep.pred = FALSE, models = FALSE)
+  }
+  save(bmr, file = "./benchmark/bmr.RData")
 }
 
